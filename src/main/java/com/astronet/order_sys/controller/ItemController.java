4d104a06662dda88365a61497dfbc7fd4b7a0844
Yanan Lyu <bigo@Yanans-MacBook-Pro.local>
2021-01-26 23:18:12 -0500

    getbyid getallAsc

--- src/main/java/com/astronet/order_sys/controller/ItemController.java
+++ src/main/java/com/astronet/order_sys/controller/ItemController.java
@@ -1,13 +1,12 @@
 package com.astronet.order_sys.controller;
 
 
+import com.astronet.order_sys.exception.ResourceNotFoundException;
 import com.astronet.order_sys.model.Item;
 import com.astronet.order_sys.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,15 @@ public class ItemController {
     // get all items
     @GetMapping("/items")
     public List<Item> getAllItems() {
-        return itemRepository.findAll();
+//        return itemRepository.findAll();
+        return itemRepository.findAllByOrderByCreateTimeAsc();
+    }
+
+    @GetMapping("items/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Item not exist with id: " + id));
+
+        return ResponseEntity.ok(item);
     }
 }
