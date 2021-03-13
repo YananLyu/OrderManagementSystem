211b677b657775897d7b98692175db8334bf32c0
Yanan Lyu <yanan.lyu@gmail.com>
2021-03-13 00:28:07 -0500

    创建Sku controller

---
+++ src/main/java/com/astronet/oms/controller/SkuController.java
@@ -0,0 +1,12 @@
+package com.astronet.oms.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Yanan Lyu
+ * @date 3/12/21 11:55 PM
+ */
+
+@RestController
+public class SkuController {
+}
--- src/main/java/com/astronet/oms/repository/PmsSkuRepository.java
+++ src/main/java/com/astronet/oms/repository/PmsSkuRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * @author: Yanan Lyu
- * @date 3/3/21 9:36 PM
+ * @author Yanan Lyu
+ * @date 3/13/21 12:05 AM
  */
+
 @Repository
 public interface PmsSkuRepository extends JpaRepository<PmsSku, Long> {
 }
